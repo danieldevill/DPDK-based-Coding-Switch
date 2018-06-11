49bb7aa9de25f26a1b7ef04cef5e7939f6e7abff
Daniel de Villiers <danieldevill@hotmail.com>
2018-06-11 17:05:22 +0200

    Started working on On-the-fly implementation.

--- l2fwd-nc/main.h
+++ l2fwd-nc/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 static void net_encode(struct rte_mbuf *m, unsigned portid, kodoc_coder_t *encoder, kodoc_coder_t *decoder);
+static void net_decode(uint8_t* payload, unsigned portid, kodoc_coder_t *encoder, kodoc_coder_t *decoder);
 
 #endif
