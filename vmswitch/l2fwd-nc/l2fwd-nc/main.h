d834f8136b5fd09478bde80d42ec1da3963aff55
Daniel de Villiers <danieldevill@hotmail.com>
2018-09-05 17:32:42 +0200

    Converted encoder to take packets from dst_addr queue. Will test tomorrow and also do decoding side.

--- vmswitch/l2fwd-nc/l2fwd-nc/main.h
+++ vmswitch/l2fwd-nc/l2fwd-nc/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-static void net_encode(struct rte_mbuf *m, unsigned portid, kodoc_coder_t *encoder, kodoc_coder_t *decoder);
+static void net_encode(kodoc_factory_t *encoder_factory);
 static void net_decode(struct rte_mbuf *m, unsigned portid, kodoc_coder_t *decoder);
-static void net_recode(struct rte_mbuf *m, unsigned portid, kodoc_coder_t *encoder, kodoc_coder_t *decoder);
-static void reset_encoder(kodoc_coder_t *encoder, kodoc_factory_t *encoder_factory);
+static void net_recode(kodoc_factory_t *encoder_factory);
 static void reset_decoder(kodoc_coder_t *decoder);
 static void coding_setup(void);
 static void update_settings(void);
