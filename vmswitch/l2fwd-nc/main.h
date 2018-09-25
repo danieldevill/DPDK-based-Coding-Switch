7f203c7929b2406e07ca1874dbb3bb65e6dc4a68
Daniel de Villiers <danieldevill@hotmail.com>
2018-09-25 17:25:48 +0200

    Wrote function to check for generationID, also added generationID to encoded packets. Need to finish decoder side. Almost there.

--- vmswitch/l2fwd-nc/main.h
+++ vmswitch/l2fwd-nc/main.h
@@ -5,7 +5,8 @@ static void net_encode(kodoc_factory_t *encoder_factory);
 static void net_decode(kodoc_factory_t *decoder_factory);
 static void net_recode(kodoc_factory_t *encoder_factory);
 static void update_settings(void);
-static void generate_generationID(char generationID[12]);
+static void generate_generationID(char *generationID);
 static struct dst_addr_status dst_mac_status(struct rte_mbuf *m, unsigned srcport); //Chcks if the dst_addr exists in the MAC table. If it does not, then it adds it to the table. 1 if it exits, 2 if it exists and is coding capable, and 0 if not.
+static void genID_in_genTable(char *generationID);
 
 #endif
